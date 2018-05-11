5d5ebc816cce4618822eb471a4fbb68fcbddc247
mxl0243 <39100956+mxl0243@users.noreply.github.com>
2018-05-11 12:12:19 -0700

    Update Carrom.java
    changed constructor to also take in the radius of the carrom

--- Carrom.java
+++ Carrom.java
@@ -3,8 +3,11 @@ public class Carrom extends Tile{
 	private int color; //red, white, black
 	private int pointVal;
 	private final int mass = 0;
+	private int carromDiameter;
 	
-	public Carrom(int color, int posX, int posY) {
+	public Carrom(int color, int posX, int posY, int carromRadius) {
+		this.color = color;
+		carromDiameter = 2*carromRadius;
 		if(color == 0)
 			pointVal = 25;
 		else if(color == 1)
@@ -22,4 +25,18 @@ public class Carrom extends Tile{
 	public int getPointVal() {
 		return pointVal;
 	}
+	
+	public void draw(Graphics g) {
+		if (color == 0) {
+			g.setColor(245, 90, 90);
+			g.fillOval(posX, posY, carromDiameter, carromDiameter);
+		} else if (color == 1) {
+			g.setColor(245, 245, 245);
+			g.fillOval(posX, posY, carromDiameter, carromDiameter);
+		} else {
+			g.setColor(0, 0, 0);
+			g.fillOval(posX, posY, carromDiameter, carromDiameter);
+		}
+	}
+			
 }
