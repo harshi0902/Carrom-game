16b42b32a1be744a4f66fa9d7aa122c2e9f45c90
mxl0243 <39100956+mxl0243@users.noreply.github.com>
2018-05-11 12:16:02 -0700

    Update Striker.java
    changed constructor to take in radius for striker

--- Striker.java
+++ Striker.java
@@ -1,9 +1,15 @@
 
 public class Striker extends Tile{
 	private final int mass = 0;
+	private int diameter;
 	
-	public Striker(int posX, int posY) {
-		
+	public Striker(int posX, int posY, int strikerRadius) {
+		super(posX, posY);
+		diameter = 2*strikerRadius;
 	}
+	
+	public void draw(Graphics g) {
+		g.setColor(245,245,245);
+		g.fillOval(posX, posY, diameter, diameter);
 
 }
