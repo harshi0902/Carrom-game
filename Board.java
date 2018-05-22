98169a2cae2002f0dc64db5e34881f18a7d7f4d5
ramyayyagari <39068855+ramyayyagari@users.noreply.github.com>
2018-05-21 17:39:22 -0700

    Add files via upload
    Updated classes

--- Board.java
+++ Board.java
@@ -8,37 +8,37 @@ import javax.imageio.ImageIO;
 
 public class Board implements Drawable{
 	ArrayList<Tile> tiles = new ArrayList<Tile>();
-	private final int carromRadius = 10;
-	private final int strikerRadius = 10;
+	private final int carromDiameter = 10;
+	private int center = 290;
 	
 	public Board() { //red = 0, white = 1, black = 2
-// 		//red
-				tiles.add(new Carrom(0, center, center));
-				
-				//white/tan
-				tiles.add(new Carrom( 1, center, center-(carromDiameter*4))); //top
-				tiles.add(new Carrom( 1, center-(carromDiameter*4), center-(carromDiameter*2))); //second row
-				tiles.add(new Carrom( 1, center+(carromDiameter*4), center-(carromDiameter*2)));
-				tiles.add(new Carrom( 1, center-(carromDiameter*2), center-(carromDiameter*1))); //third row
-				tiles.add(new Carrom( 1, center+(carromDiameter*2), center-(carromDiameter*1)));
-				tiles.add(new Carrom( 1, center-(carromDiameter*4), center+(carromDiameter*2))); //fourth row
-				tiles.add(new Carrom( 1, center, center+(carromDiameter*2)));
-				tiles.add(new Carrom( 1, center+(carromDiameter*4), center+(carromDiameter*2)));
-				tiles.add(new Carrom( 1, center, center+(carromDiameter*4))); //bottom
-				
-				//black
-				tiles.add(new Carrom( 2, center-(carromDiameter*2), center-(carromDiameter*3))); //first row
-				tiles.add(new Carrom( 2, center+(carromDiameter*2), center-(carromDiameter*3)));
-				tiles.add(new Carrom( 2, center, center-(carromDiameter*2))); //second row
-				tiles.add(new Carrom( 2, center-(carromDiameter*4), center)); //third row
-				tiles.add(new Carrom( 2, center+(carromDiameter*4), center));
-				tiles.add(new Carrom( 2, center-(carromDiameter*2), center+carromDiameter)); //third row
-				tiles.add(new Carrom( 2, center+(carromDiameter*2), center+carromDiameter));
-				tiles.add(new Carrom( 2, center-(carromDiameter*2), center+(carromDiameter*3))); // third row
-				tiles.add(new Carrom( 2, center+(carromDiameter*2), center+(carromDiameter*3)));
-				
+		//red
+		tiles.add(new Carrom(0, center, center));
+		
+		//white/tan
+		tiles.add(new Carrom( 1, center, center-(carromDiameter*4))); //top
+		tiles.add(new Carrom( 1, center-(carromDiameter*4), center-(carromDiameter*2))); //second row
+		tiles.add(new Carrom( 1, center+(carromDiameter*4), center-(carromDiameter*2)));
+		tiles.add(new Carrom( 1, center-(carromDiameter*2), center-(carromDiameter*1))); //third row
+		tiles.add(new Carrom( 1, center+(carromDiameter*2), center-(carromDiameter*1)));
+		tiles.add(new Carrom( 1, center-(carromDiameter*4), center+(carromDiameter*2))); //fourth row
+		tiles.add(new Carrom( 1, center, center+(carromDiameter*2)));
+		tiles.add(new Carrom( 1, center+(carromDiameter*4), center+(carromDiameter*2)));
+		tiles.add(new Carrom( 1, center, center+(carromDiameter*4))); //bottom
+		
+		//black
+		tiles.add(new Carrom( 2, center-(carromDiameter*2), center-(carromDiameter*3))); //first row
+		tiles.add(new Carrom( 2, center+(carromDiameter*2), center-(carromDiameter*3)));
+		tiles.add(new Carrom( 2, center, center-(carromDiameter*2))); //second row
+		tiles.add(new Carrom( 2, center-(carromDiameter*4), center)); //third row
+		tiles.add(new Carrom( 2, center+(carromDiameter*4), center));
+		tiles.add(new Carrom( 2, center-(carromDiameter*2), center+carromDiameter)); //third row
+		tiles.add(new Carrom( 2, center+(carromDiameter*2), center+carromDiameter));
+		tiles.add(new Carrom( 2, center-(carromDiameter*2), center+(carromDiameter*3))); // third row
+		tiles.add(new Carrom( 2, center+(carromDiameter*2), center+(carromDiameter*3)));
+
 		//fourth row
-		tiles.add(new Striker(center, 470));
+		tiles.add(new Striker(center, 480));
 	}
 	
 	@Override
@@ -50,8 +50,15 @@ public class Board implements Drawable{
 			System.out.println("bleh");
 		}
 		g.drawImage(img, 0, 0, 600, 600, null);
+		
+		for(Tile t: tiles) {
+			t.draw(g);
+		}
 	}
 	
+	public ArrayList<Tile> getTiles(){
+		return tiles;
+	}
 	public int strikerIndex() {
 		for(int i = 0; i < tiles.size(); i++) {
 			if(tiles.get(i) instanceof Striker)
--- Carrom.java
+++ Carrom.java
@@ -1,21 +1,26 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Carrom extends Tile{
 	private int color; //red, white, black
 	private int pointVal;
-	private final int mass = 0;
-	private int carromDiameter;
+	private final int carromDiameter = 20;
+	private Color c;
 	
-	public Carrom(int color, int posX, int posY, int carromRadius) {
-		this.color = color;
-		carromDiameter = 2*carromRadius;
-		if(color == 0)
+	public Carrom(int color, int posX, int posY) {
+		super(posX, posY, 0, 0);
+		if(color == 0) {
 			pointVal = 25;
-		else if(color == 1)
+			c = new Color(245, 90, 90);
+		}
+		else if(color == 1) {
 			pointVal = 10;
+			c = new Color(240, 215, 180);
+		}
 		else {
 			pointVal = 5;
+			c = new Color(0, 0, 0);
 		}
-		super(posX, posY);
 	}
 
 	public int getColor() {
@@ -26,21 +31,9 @@ public class Carrom extends Tile{
 		return pointVal;
 	}
 	
+	@Override
 	public void draw(Graphics g) {
-		if (color == 0) {
-			g.setColor(new Color(245, 90, 90));
-			g.fillOval(posX, posY, carromDiameter, carromDiameter);
-			g.setColor(new Color(200, 0, 0));
-			g.drawOval(posX, posY, carromDiameter, carromDiameter);
-		} else if (color == 1) {
-			g.setColor(245, 245, 245);
-			g.fillOval(posX, posY, carromDiameter, carromDiameter);
-			g.setColor(new Color(0, 0, 0));
-			g.drawOval(posX, posY, carromDiameter, carromDiameter);
-		} else {
-			g.setColor(0, 0, 0);
-			g.fillOval(posX, posY, carromDiameter, carromDiameter);
-		}
+		g.setColor(c);
+		g.fillOval(super.getX(), super.getY(), carromDiameter, carromDiameter); //position
 	}
-			
 }
