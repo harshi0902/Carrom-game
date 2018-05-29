08a3b95f7b7c3e03a75478b5c2f1fcc14223897e
Shreyas <kallingal.shreyas@gmail.com>
2018-05-28 20:06:04 -0700

    Update Striker.java

--- Striker.java
+++ Striker.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Striker extends Tile{
+public class Striker extends Tile {
 	private final int strikerDiameter = 23;
 	private boolean isClicked;
 	private boolean isHighlighted;
@@ -20,47 +20,48 @@ public class Striker extends Tile{
 		isClicked = false;
 		isHighlighted = false;
 		try {
-		    img = ImageIO.read(getClass().getResource("line.png")); //name of file of picture of board
+			img = ImageIO.read(getClass().getResource("line.png")); // name of
+																	// file of
+																	// picture
+																	// of board
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int getCenterX() {
-		super.setCenterX(super.getX() + (strikerDiameter/2));  
+		super.setCenterX(super.getX() + (strikerDiameter / 2));
 		return super.getCenterX();
 	}
-	
+
 	public int getCenterY() {
-		super.setCenterY(super.getY() + (strikerDiameter/2));  
+		super.setCenterY(super.getY() + (strikerDiameter / 2));
 		return super.getCenterY();
 	}
-	
-	public double getRadius() {
-		return strikerDiameter/(double)2;
-	}
-	
+
 	public boolean isClicked() {
 		return isClicked;
 	}
-	
+
 	public int getDiameter() {
 		return strikerDiameter;
 	}
-	
+
 	public void draw(Graphics g) {
-		if(!isHighlighted) {
-			g.setColor(new Color(245, 245, 245)); //white
-			g.fillOval(super.getX(), super.getY(), strikerDiameter, strikerDiameter);//position + size
+		if (!isHighlighted) {
+			g.setColor(new Color(245, 245, 245)); // white
+			g.fillOval(super.getX(), super.getY(), strikerDiameter, strikerDiameter);// position
+																						// +
+																						// size
 		}
-		
+
 		else {
 			g.setColor(new Color(250, 250, 10));
 			g.drawOval(super.getX(), super.getY(), strikerDiameter, strikerDiameter);
 			strikerTrans.setToTranslation(super.getCenterX(), super.getCenterY());
 			strikerTrans.scale(0.05, 0.05);
 			strikerTrans.rotate(-super.getDir());
-			Graphics2D g2 = (Graphics2D)g;
+			Graphics2D g2 = (Graphics2D) g;
 			g2.drawImage(img, strikerTrans, null);
 		}
 	}
@@ -73,16 +74,17 @@ public class Striker extends Tile{
 		isHighlighted = false;
 	}
 
+	public double getRadius() {
+		return strikerDiameter/(double)2;
+	}
 	public void incrementAngle(double d) {
 		double angle = super.getDir();
 		super.setDir(angle += d);
-		if(super.getDir()>=2*Math.PI){
-			super.setDir(super.getDir()-2*Math.PI);
+		if (super.getDir() >= 2 * Math.PI) {
+			super.setDir(super.getDir() - 2 * Math.PI);
+		} else if (super.getDir() <= 0) {
+			super.setDir(2 * Math.PI - super.getDir());
 		}
-		else if(super.getDir()<= 0){
-			super.setDir(2*Math.PI-super.getDir());
-		}
-		System.out.println(super.getDir());
 	}
 
 }
