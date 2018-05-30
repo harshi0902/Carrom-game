20eff7015d1b23c299c8dab6c7b1b6040a70b3e1
ramyayyagari <39068855+ramyayyagari@users.noreply.github.com>
2018-05-29 20:26:15 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -11,36 +11,17 @@ public class Board implements Drawable{
 	private final int carromDiameter = 10;
 	private int center = 290;
 	
-	public Board() { //red = 0, white = 1, black = 2
-		//red
-		tiles.add(new Carrom(0, center, center));
-		
-		//white/tan
-		tiles.add(new Carrom( 1, center, center-(carromDiameter*4))); //top
-		tiles.add(new Carrom( 1, center-(carromDiameter*4), center-(carromDiameter*2))); //second row
-		tiles.add(new Carrom( 1, center+(carromDiameter*4), center-(carromDiameter*2)));
-		tiles.add(new Carrom( 1, center-(carromDiameter*2), center-(carromDiameter*1))); //third row
-		tiles.add(new Carrom( 1, center+(carromDiameter*2), center-(carromDiameter*1)));
-		tiles.add(new Carrom( 1, center-(carromDiameter*4), center+(carromDiameter*2))); //fourth row
-		tiles.add(new Carrom( 1, center, center+(carromDiameter*2)));
-		tiles.add(new Carrom( 1, center+(carromDiameter*4), center+(carromDiameter*2)));
-		tiles.add(new Carrom( 1, center, center+(carromDiameter*4))); //bottom
-		
-		//black
-		tiles.add(new Carrom( 2, center-(carromDiameter*2), center-(carromDiameter*3))); //first row
-		tiles.add(new Carrom( 2, center+(carromDiameter*2), center-(carromDiameter*3)));
-		tiles.add(new Carrom( 2, center, center-(carromDiameter*2))); //second row
-		tiles.add(new Carrom( 2, center-(carromDiameter*4), center)); //third row
-		tiles.add(new Carrom( 2, center+(carromDiameter*4), center));
-		tiles.add(new Carrom( 2, center-(carromDiameter*2), center+carromDiameter)); //third row
-		tiles.add(new Carrom( 2, center+(carromDiameter*2), center+carromDiameter));
-		tiles.add(new Carrom( 2, center-(carromDiameter*2), center+(carromDiameter*3))); // third row
-		tiles.add(new Carrom( 2, center+(carromDiameter*2), center+(carromDiameter*3)));
-
-		//fourth row
+	public Board() { 
+		generateNewTile();
 		tiles.add(new Striker(center, 480));
 	}
 	
+	public void generateNewTile() {
+		int x = (int)((Math.random() * 503) + 36);
+		int y = (int)((Math.random() * 503) + 36);
+		tiles.add(new Carrom(2, x, y));
+	}
+
 	@Override
 	public void draw(Graphics g) {
 		BufferedImage img = null;
@@ -59,12 +40,17 @@ public class Board implements Drawable{
 	public ArrayList<Tile> getTiles(){
 		return tiles;
 	}
+	
 	public int strikerIndex() {
 		for(int i = 0; i < tiles.size(); i++) {
 			if(tiles.get(i) instanceof Striker)
 				return i;
 		}
 		return -1;
+	}
+	
+	public void reset() {
+		
 	}
 
 }
