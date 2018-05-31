9c8319da749ba15ba6f59a00f700d0cc2693765f
ramyayyagari <39068855+ramyayyagari@users.noreply.github.com>
2018-05-30 19:36:12 -0700

    Update CarromBoardPanel.java

--- CarromBoardPanel.java
+++ CarromBoardPanel.java
@@ -173,7 +173,7 @@ public class CarromBoardPanel extends JPanel {
 	}
 
 	public static class Action1 implements ActionListener { // for the first
-															// button
+		// button
 		public void actionPerformed(ActionEvent e) {
 			JFrame frame2 = new JFrame("RULES PAGE"); // name for the frame
 			frame2.setVisible(true);
@@ -199,7 +199,7 @@ public class CarromBoardPanel extends JPanel {
 			Icon imgIcon = new ImageIcon(this.getClass().getResource("rules.gif"));
 			label.setIcon(imgIcon);
 			label.setBounds(668, 43, 46, 14); // for example, you can use your
-												// own values
+			// own values
 			frame2.getContentPane().add(label);
 		}
 
@@ -213,7 +213,7 @@ public class CarromBoardPanel extends JPanel {
 
 					Tile one = tiles.get(i);
 					Tile two = tiles.get(j);
-					
+
 					int difY = Math.abs(two.getCenterY() - one.getCenterY());
 
 					double angle = Math.asin((difY) / (one.getRadius() +
@@ -230,26 +230,26 @@ public class CarromBoardPanel extends JPanel {
 
 					double difAngleOne = one.getDir() - angleSlope;
 					double difAngleTwo = two.getDir() - angleSlope;
-					
+
 					double newAngleOne = Math.PI - difAngleOne;
 					if (Math.abs(newAngleOne) >= 2 * Math.PI) {
 						newAngleOne = Math.abs(newAngleOne) % (2 * Math.PI);
 					} else if (newAngleOne <= 0) {
 						newAngleOne = 2 * Math.PI + newAngleOne;
 					}
-					
+
 					double newAngleTwo = Math.PI - difAngleTwo;
 					if (Math.abs(newAngleTwo) >= 2 * Math.PI) {
 						newAngleTwo = Math.abs(newAngleTwo) % (2 * Math.PI);
 					} else if (newAngleTwo <= 0) {
 						newAngleTwo = 2 * Math.PI + newAngleTwo;
 					}
-					
+
 					one.setDir(newAngleOne);
 					hit(one);
 					one.setSpeed(15);
 					System.out.println("one: " + one.getDir());
-					
+
 					two.setDir(newAngleTwo);
 					hit(two);
 					one.setSpeed(15);
@@ -257,7 +257,7 @@ public class CarromBoardPanel extends JPanel {
 				}
 			}
 		}
-		
+
 		for(Tile ti : tiles) {
 			if (ti.getX() >= 523 && ti.getY() <= 70 || ti.getX() <= 75 && ti.getY() <= 70 || ti.getX() >= 523 && ti.getY() >= 517
 					|| ti.getX() <= 75 && ti.getY() >= 517) {
@@ -288,14 +288,9 @@ public class CarromBoardPanel extends JPanel {
 					ti.setDir((Math.PI * 3) / 2);
 
 				}
+
 				else {
-					double newAngle = (2*Math.PI) - ti.getDir();
-					if (Math.abs(newAngle) >= 2 * Math.PI) {
-						newAngle = Math.abs(newAngle) % (2 * Math.PI);
-					} else if (newAngle <= 0) {
-						newAngle = (2*Math.PI) + newAngle;
-					}
-					ti.setDir(newAngle);
+					ti.setDir((2 * Math.PI) - ti.getDir());
 				}
 
 				ti.setTime(0);
@@ -316,16 +311,11 @@ public class CarromBoardPanel extends JPanel {
 				if (roundDir == verPi) {
 					ti.setDir(Math.PI / 2);
 				} else {
-					double newAngle = (2*Math.PI) - ti.getDir();
-					if (Math.abs(newAngle) >= 2 * Math.PI) {
-						newAngle = Math.abs(newAngle) % (2 * Math.PI);
-					} else if (newAngle <= 0) {
-						newAngle = (2*Math.PI) + newAngle;
-					}
-					ti.setDir(newAngle);
+					ti.setDir(2 * Math.PI - ti.getDir());
 				}
 				ti.setTime(0);
 				hit(ti);
+				System.out.println(ti.getDir());
 
 			}
 
@@ -342,13 +332,12 @@ public class CarromBoardPanel extends JPanel {
 				if (roundDir == horPi) {
 					ti.setDir(0);
 				} else {
-					double newAngle = (Math.PI) - ti.getDir();
-					if (Math.abs(newAngle) >= 2 * Math.PI) {
-						newAngle = Math.abs(newAngle) % (2 * Math.PI);
-					} else if (newAngle <= 0) {
-						newAngle = (2*Math.PI) + newAngle;
+					if(Math.PI- ti.getDir() < 0){
+						ti.setDir(Math.PI - ti.getDir() + (2*Math.PI));
 					}
-					ti.setDir(newAngle);
+					else{ti.setDir((Math.PI) - ti.getDir());
+
+					}
 				}
 				ti.setTime(0);
 				hit(ti);
@@ -362,156 +351,158 @@ public class CarromBoardPanel extends JPanel {
 
 				if (roundDir == horPi) {
 					ti.setDir(Math.PI);
-				} else {
-					double newAngle = (Math.PI) - ti.getDir();
-					if (Math.abs(newAngle) >= 2 * Math.PI) {
-						newAngle = Math.abs(newAngle) % (2 * Math.PI);
-					} else if (newAngle <= 0) {
-						newAngle = (2*Math.PI) + newAngle;
+				} 
+				else {
+					if(Math.PI- ti.getDir() < 0){
+						ti.setDir(Math.PI - ti.getDir() + (2*Math.PI));
 					}
-					ti.setDir(newAngle);
+					else{ti.setDir((Math.PI) - ti.getDir());
+
+					}
 				}
 
 				ti.setTime(0);
 				hit(ti);
 				for(Integer[] in : ti.getPath()){
-					System.out.print("(" + in[0] + ", " + in[1] + ")" + "    ");
+					System.out.print("(" + in[0] + ", " + in[1] + ")" + "    ");
 				}
 			}
-		}
-		for (Tile ti : tiles) {
 
-			if (ti.getPath().size() > (ti.getTime() + (int) ti.getSpeed())) {
-				ti.move(ti.getPath().get(ti.getTime() + (int) ti.getSpeed())[0],
-						ti.getPath().get(ti.getTime() + (int) ti.getSpeed())[1]);
-				ti.setTime(ti.getTime() + (int) ti.getSpeed());
-				if (ti.getSpeed() - 0.1 > 0) {
-					ti.setSpeed(ti.getSpeed() - 0.1);
-				}
-
-			}
 		}
 
-		System.out.println(s.getDir());
-		System.out.println("speed: " + s.getSpeed());
-		frame.repaint();
+	for (Tile ti : tiles) {
 
-	}
-
-	public static void restart() {
-		board = new Board();
-		tiles = board.getTiles();
-		s = (Striker) tiles.get(board.strikerIndex());
-		frame.repaint();
-	}
-	
-	public boolean collision(Tile t, Tile t2) { // is a tile touching another
-												// tile
-		double distance = Math.hypot(t.getCenterX() - t2.getCenterX(), t.getCenterY() - t2.getCenterY());
-
-		distance *= 10;
-		distance = Math.round(distance);
-		distance /= 10;
-		double thisRadius = t.getRadius();
-		double otherRadius = t2.getRadius();
-		if (distance <= otherRadius + thisRadius) {
-			return true;
-		}
-		return false;
-	}
-
-	public static void hit(Tile mTile) {
-		System.out.println("called hit");
-		if (mTile.getSpeed() > 1) {
-			mTile.setSpeed(mTile.getSpeed() - 1);
-		}
-		mTile.clearPath();
-
-		double dir = mTile.getDir();
-		int x = mTile.getX();
-		int y = mTile.getY();
-		double ver = dir * 100;
-		ver = Math.round(ver);
-		ver = ver / 100;
-
-		double verPi = Math.PI / 2;
-		verPi = verPi * 100;
-		verPi = Math.round(verPi);
-		verPi = verPi / 100;
-
-		double verPi2 = 3 * Math.PI / 2;
-		verPi2 = verPi2 * 100;
-		verPi2 = Math.round(verPi2);
-		verPi2 = verPi2 / 100;
-
-		if (ver == verPi) {
-			int ct = 0;
-			System.out.println("its the one");
-			for (int i = y; i >= 0; i--) {
-				Integer[] coord = new Integer[2];
-				coord[0] = new Integer(x);
-				Integer proY = new Integer(y - ct);
-				coord[1] = proY;
-				mTile.setPath(ct, coord);
-				ct++;
-			}
-		}
-
-		else if (ver == verPi2) {
-			int ct = 0;
-
-			for (int i = y; i < 566; i++) {
-				Integer[] coord = new Integer[2];
-				coord[0] = new Integer(x);
-				Integer proY = new Integer(y + ct);
-				coord[1] = proY;
-				mTile.setPath(ct, coord);
-				ct++;
-			}
-		}
-
-		else if ((dir >= 0 && dir < Math.PI / 2) || (dir > Math.PI * 3 / 2 && dir < Math.PI * 2)) {
-			int count = 565;
-			int ct = 0;
-			for (int i = x; i < count; i++) {
-				Integer[] coord = new Integer[2];
-				coord[0] = new Integer(i);
-				Integer proY;
-				proY = new Integer(y - (int) (ct * Math.tan(dir)));
-				coord[1] = proY;
-
-				mTile.setPath(ct, coord);
-				ct++;
+		if (ti.getPath().size() > (ti.getTime() + (int) ti.getSpeed())) {
+			ti.move(ti.getPath().get(ti.getTime() + (int) ti.getSpeed())[0],
+					ti.getPath().get(ti.getTime() + (int) ti.getSpeed())[1]);
+			ti.setTime(ti.getTime() + (int) ti.getSpeed());
+			if (ti.getSpeed() - 0.1 > 0) {
+				ti.setSpeed(ti.getSpeed() - 0.1);
 			}
 
 		}
+	}
 
-		else if ((dir > Math.PI / 2 && dir <= Math.PI) || (dir > Math.PI && dir < Math.PI * 3 / 2)) {
-			int count = 0;
-			int ct = 0;
-			for (int i = x; i > count; i--) {
-				Integer[] coord = new Integer[2];
-				coord[0] = new Integer(i);
-				Integer proY = new Integer(y - (int) (ct * Math.tan(Math.PI - dir)));
-				coord[1] = proY;
-				mTile.setPath(ct, coord);
-				ct++;
-			}
+	System.out.println(s.getDir());
+	System.out.println("speed: " + s.getSpeed());
+	frame.repaint();
+
+}
+
+public static void restart() {
+	board = new Board();
+	tiles = board.getTiles();
+	s = (Striker) tiles.get(board.strikerIndex());
+	frame.repaint();
+}
+
+public boolean collision(Tile t, Tile t2) { // is a tile touching another
+	// tile
+	double distance = Math.hypot(t.getCenterX() - t2.getCenterX(), t.getCenterY() - t2.getCenterY());
+
+	distance *= 10;
+	distance = Math.round(distance);
+	distance /= 10;
+	double thisRadius = t.getRadius();
+	double otherRadius = t2.getRadius();
+	if (distance <= otherRadius + thisRadius) {
+		return true;
+	}
+	return false;
+}
+
+public static void hit(Tile mTile) {
+	System.out.println("called hit");
+	if (mTile.getSpeed() > 1) {
+		mTile.setSpeed(mTile.getSpeed() - 1);
+	}
+	mTile.clearPath();
+
+	double dir = mTile.getDir();
+	int x = mTile.getX();
+	int y = mTile.getY();
+	double ver = dir * 100;
+	ver = Math.round(ver);
+	ver = ver / 100;
+
+	double verPi = Math.PI / 2;
+	verPi = verPi * 100;
+	verPi = Math.round(verPi);
+	verPi = verPi / 100;
+
+	double verPi2 = 3 * Math.PI / 2;
+	verPi2 = verPi2 * 100;
+	verPi2 = Math.round(verPi2);
+	verPi2 = verPi2 / 100;
+
+	if (ver == verPi) {
+		int ct = 0;
+		System.out.println("its the one");
+		for (int i = y; i >= 0; i--) {
+			Integer[] coord = new Integer[2];
+			coord[0] = new Integer(x);
+			Integer proY = new Integer(y - ct);
+			coord[1] = proY;
+			mTile.setPath(ct, coord);
+			ct++;
 		}
+	}
 
+	else if (ver == verPi2) {
+		int ct = 0;
+
+		for (int i = y; i < 566; i++) {
+			Integer[] coord = new Integer[2];
+			coord[0] = new Integer(x);
+			Integer proY = new Integer(y + ct);
+			coord[1] = proY;
+			mTile.setPath(ct, coord);
+			ct++;
+		}
+	}
+
+	else if ((dir >= 0 && dir < Math.PI / 2) || (dir > Math.PI * 3 / 2 && dir < Math.PI * 2)) {
+		int count = 565;
+		int ct = 0;
+		for (int i = x; i < count; i++) {
+			Integer[] coord = new Integer[2];
+			coord[0] = new Integer(i);
+			Integer proY;
+			proY = new Integer(y - (int) (ct * Math.tan(dir)));
+			coord[1] = proY;
+
+			mTile.setPath(ct, coord);
+			ct++;
+		}
 
 	}
 
-	public void paintComponent(Graphics g) {
-		board.draw(g);
-		g.setColor(Color.white);
-		g.setFont(new Font("Montserrat", Font.PLAIN, 20));
-		g.drawString("Score: " + score, 400, 30);
-		if(gameOver) {
-			g.fillRect(240, 260, 140, 70);
-			g.setColor(Color.BLACK);
-			g.drawString("GAME OVER", 250, 300);
+	else if ((dir > Math.PI / 2 && dir <= Math.PI) || (dir > Math.PI && dir < Math.PI * 3 / 2)) {
+		int count = 0;
+		int ct = 0;
+		for (int i = x; i > count; i--) {
+			Integer[] coord = new Integer[2];
+			coord[0] = new Integer(i);
+			Integer proY = new Integer(y - (int) (ct * Math.tan(Math.PI - dir)));
+			coord[1] = proY;
+			mTile.setPath(ct, coord);
+			ct++;
 		}
 	}
+
+
+}
+
+public void paintComponent(Graphics g) {
+	board.draw(g);
+	g.setColor(Color.white);
+	g.setFont(new Font("Montserrat", Font.PLAIN, 20));
+	g.drawString("Score: " + score, 400, 30);
+	if(gameOver) {
+		g.fillRect(240, 260, 140, 70);
+		g.setColor(Color.BLACK);
+		g.drawString("GAME OVER", 250, 300);
+	}
+}
 
 }
